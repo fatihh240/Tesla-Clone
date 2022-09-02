8fdbcb14cc83668bfcb103e6c70eff8d09890dbd
Fatih Menteşe <fa34mehmet@hotmail.com>
2022-09-02 05:06:33 +0300

    Tesla Clone
    2022

---
+++ tesla/src/components/Section.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import styled from 'styled-components'
+import Fade from 'react-reveal/Fade';
+
+
+function Section({ title, description, backgroundImg, leftBtnText, rightBtnText }) {
+  return (
+    <Wrap bgImage={backgroundImg}>
+         <Fade bottom>
+            <ItemText>
+                <h1>{ title }</h1>
+                <p>{ description }</p>
+            </ItemText>
+        </Fade>
+        <Buttons>
+            <Fade bottom>
+            <ButtonGroup>
+                <LeftButton>
+                    {leftBtnText}
+                </LeftButton>
+                {rightBtnText && 
+                <RightButton>
+                    {rightBtnText}
+                </RightButton>
+                }
+            </ButtonGroup>
+        </Fade>
+        <DownArrow src="/images/down-arrow.svg" />
+        </Buttons>
+    </Wrap>
+  )
+}
+
+export default Section
+
+const Wrap = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: orange;
+    background-size: cover;
+    background-position: center;
+    backgroun-repeat: no-repeat;
+    background-image: url("/images/model-s.jpg");
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between; // vertial
+    align-items: center; // horizontal
+    background-image: ${props => `url("/images/${props.bgImage}")` }
+`
+const ItemText = styled.div`
+    text-align: center;
+    padding-top: 15vh;
+`
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
+
+`
+
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor: pointer;
+    margin: 8px;
+
+`
+
+const RightButton = styled(LeftButton)`
+    background: white;
+    opacity: 0.85;
+    color: black;
+
+`
+
+const DownArrow = styled.img`
+    margin-top: 20px;
+    height: 40px;
+    overflow-x: hidden;
+    animation: animateDown infinite 1.5s;
+`
+const Buttons = styled.div`
+
+
+`
